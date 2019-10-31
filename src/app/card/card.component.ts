1f9021d24b1e5a8f07df3fa5874842b1e5feefd5
Guilherme Teixeira <guisebastian15@gamil.com>
2019-10-31 02:43:31 -0300

    altera cor dos botoes ao clicar

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,24 +7,35 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() id: number;
   @Input() titulo: string;
   @Input() nInscritos: string;
   @Input() nPublicacoes: string;
+  @Input() image: string;
 
-  acao = 'Desinscrever';
-  icon = 'remove';
+  card_button: HTMLElement;
+  acao = 'Inscrever';
+  icon = 'add';
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() { }
 
   clickInscreverButton = () => {
+    this.card_button = document.getElementById("card-button-" + String(this.id)) as HTMLElement;
+
     if (this.acao === 'Desinscrever') {
       this.acao = 'Inscrever';
       this.icon = 'add';
+      this.card_button.style.color = '#008639';
+      this.card_button.style.borderColor = '#008639';
     } else {
       this.acao = 'Desinscrever';
       this.icon = 'remove';
+      this.card_button.style.color = '#EE6611';
+      this.card_button.style.borderColor = '#EE6611';
     }
   }
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,17 +8,20 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   noticias = [{
+    id: 0,
     titulo: 'Restaurante Universitário',
     nPublicacoes: 1,
-    nInscritos: 2
+    nInscritos: 2,
+    image: '../assets/icon/restaurante-universitario.jpg'
   },
     {
+      id: 1,
       titulo: 'PRG UFLA',
       nPublicacoes: 2,
-      nInscritos: 3
+      nInscritos: 3,
+      image: 'https://img.elo7.com.br/product/zoom/13F22D3/2-bases-quadradas-com-velas-quadradas-castical-pequeno.jpg'
     }];
 
-  constructor() {
-  }
+  constructor() {}
 
 }
