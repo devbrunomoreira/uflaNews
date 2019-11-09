3b72841928f3b478b360c5a5739d68fac6bb690e
gureis <gureis4@gmail.com>
2019-11-09 17:49:09 -0300

    consumindo listagem de publicadores @ home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,29 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PublicadoresModel } from '../models/publicadores.model';
+import { PublicadoresService } from '../services/publicadores.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  publicadores = [
-    {
-      id: 0,
-      titulo: 'Restaurante Universitário',
-      nPublicacoes: 1,
-      nInscritos: 2,
-      image: '../assets/icon/restaurante-universitario.jpg'
-    },
-    {
-      id: 1,
-      titulo: 'PRG UFLA',
-      nPublicacoes: 2,
-      nInscritos: 3,
-      image: 'https://img.elo7.com.br/product/zoom/13F22D3/2-bases-quadradas-com-velas-quadradas-castical-pequeno.jpg'
-    }
-  ];
+  listaDePublicadores: PublicadoresModel[];
 
-  constructor() {}
+  constructor(public publicadoresService: PublicadoresService) { }
 
+  async ngOnInit() {
+    this.listaDePublicadores = await this.publicadoresService.getAll();
+  } 
 }
