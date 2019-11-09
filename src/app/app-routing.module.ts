f983ae07cb1d81203c95ec02ecfdaa8f3ab2083d
Guilherme Teixeira <guisebastian15@gamil.com>
2019-11-09 00:42:57 -0300

    criacao das telas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'publicador', loadChildren: './publicador/publicador.module#PublicadorPageModule' },
   { path: 'publicacao', loadChildren: './publicacao/publicacao.module#PublicacaoPageModule' },
+  { path: 'usuario', loadChildren: './usuario/usuario.module#UsuarioPageModule' },
+  { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 
 ];
 
