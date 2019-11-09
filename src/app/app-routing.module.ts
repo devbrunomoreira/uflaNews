f86becbf91a00606a53343cd3ca8b42edd49cb5b
gureis <gureis4@gmail.com>
2019-11-09 17:50:13 -0300

    criacao de rotas dinamicas para publicacoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'publicador', loadChildren: './publicador/publicador.module#PublicadorPageModule' },
-  { path: 'publicacao', loadChildren: './publicacao/publicacao.module#PublicacaoPageModule' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'publicador', loadChildren: './publicador/publicador.module#PublicadorPageModule' },
+  { path: 'publicacao/:id', loadChildren: './publicacao/publicacao.module#PublicacaoPageModule' },
   { path: 'usuario', loadChildren: './usuario/usuario.module#UsuarioPageModule' },
   { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
