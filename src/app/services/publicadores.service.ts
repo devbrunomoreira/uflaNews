c6e4f27bf23d4a82b588c3e09470cea10de9f9d1
gureis <gureis4@gmail.com>
2019-11-09 17:47:53 -0300

    criado servico de listagem de publicadores

---
+++ src/app/services/publicadores.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { PublicadoresModel } from '../models/publicadores.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from "rxjs/operators";
+import { AuthService } from './auth.service';
+
+const API_URL: string = "http://localhost:3000";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PublicadoresService {
+
+
+    constructor(public http: HttpClient, public authService: AuthService) { }
+
+    async getHttpOptions() {
+        const token = await this.authService.getAuthToken();
+
+        const options = {
+            headers: new HttpHeaders({
+                'Authorization': `Bearer ${token}`
+            })
+        };
+
+        return options;
+    }
+
+    async getAll(): Promise<PublicadoresModel[]> {
+        const options = await this.getHttpOptions();        
+
+        return this.http.get(`${API_URL}/publicadores`, options).pipe(
+            map(
+                (itens: PublicadoresModel[]) => {
+                    return itens.map(
+                        (item: PublicadoresModel) => {
+                            return new PublicadoresModel(
+                                item.id,
+                                item.titulo,
+                                item.publicacoes,
+                                item.nInscritos,
+                                item.image
+                            );
+                        }
+                    )
+                }
+            )
+        ).toPromise();
+    }
+}
